4a5e99fae0e57711a6b40b46c7346f5eef580a5d
Tomasz Krutowski <tkrutowski@gmail.com>
2022-04-19 12:45:06 +0200

    display users view: TheUsers

---
+++ src/views/UsersView.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <TheUsers/>
+  </div>
+</template>
+
+<script>
+import TheUsers from "@/components/user/TheUsers.vue";
+// @ is an alias to /src
+// import Menu from '@/components/TheHeader.vue';
+
+export default {
+  name: 'UserView',
+  components: {
+    TheUsers
+},
+};
+</script>
